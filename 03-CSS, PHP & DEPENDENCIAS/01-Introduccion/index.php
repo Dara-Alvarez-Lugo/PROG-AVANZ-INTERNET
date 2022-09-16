78fbfe8dc42ca9b5898837607ba9e2997651525b
Dara-Alvarez-lugo <daraelienaialvarezlugo@gmail.com>
2022-09-15 18:45:41 -0600

    Trabajo

---
+++ 03-CSS, PHP & DEPENDENCIAS/01-Introduccion/index.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>CSS, PHP & DEPENDENCIAS</title>
+    <link rel="stylesheet" href="public/css/main.css">
+</head>
+<body>
+
+<div class="container">
+
+    <fieldset>
+
+            <legend>
+                Acceso al panel
+            </legend>
+        <form>
+
+            <label for="">Correo electronico</label>
+            <input type="email" name="">
+
+            <label for="">Contraseña</label>
+            <input type="password" name="">
+
+            <button type="submit">Acceder</button>
+
+
+        </form>
+        
+    </fieldset>
+
+
+
+</div>
+    
+</body>
+</html>
