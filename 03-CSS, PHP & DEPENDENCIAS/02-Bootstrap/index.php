79274641ca61ac551746095d3106f341c6214419
Dara-Alvarez-Lugo <daraelienaialvarezlugo@gmail.com>
2022-09-21 16:51:51 -0600

    Trabajo

---
+++ 03-CSS, PHP & DEPENDENCIAS/02-Bootstrap/index.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>CSS, PHP & DEPENDENCIAS</title>
+    <link
+      href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css"
+      rel="stylesheet"
+      integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx"
+      crossorigin="anonymous"
+    />
+
+</head>
+<body>
+
+<div class="container">
+
+    <section>
+        <div class="row registro justify-content-md-center">
+            <div class="col-md-6 col-sm-12">
+
+                <form>
+                    <h1>Acceso al panel</h1>
+
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Obcaecati doloremque perferendis non esse veniam, officiis at, tempore quas corporis pariatur, ut sapiente doloribus nesciunt. Fugit voluptates ea cumque esse? Soluta.</p>
+
+                    <label for="">Username</label>
+                    <div class="input-group mb-3">
+                        <span class="input-group-text" id="basic-addon1">@</span>
+                        <input type="text" class="form-control" placeholder="Username" aria-label="Username" aria-describedby="basic-addon1">
+                    </div>
+
+                    <label for="">Password</label>
+                    <div class="input-group mb-3">
+                        <span class="input-group-text" id="basic-addon1">@</span>
+                        <input type="text" class="form-control" placeholder="password" aria-label="Username" aria-describedby="basic-addon1">
+                    </div>
+
+                    <button class="btn btn-primary col-12" type="submit">Acceder</button>
+
+
+                </form>
+
+            </div>
+
+        </div>
+    </section>
+
+
+</div>
+    
+</body>
+</html>
